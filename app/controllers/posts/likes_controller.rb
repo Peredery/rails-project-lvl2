6047c149abf41c79098452b998c6dd43b1ef5645
A.V. Peredery <peredery@pm.me>
2024-02-08 17:31:44 +0500

    prevent multiple likes

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -5,10 +5,10 @@ class Posts::LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @like = @post.likes.build(user: current_user)
+    @like = @post.likes.find_or_create_by(user: current_user)
 
     respond_to do |format|
-      if @like.save
+      if @like.new_record?
         format.turbo_stream { render turbo_stream: turbo_stream.replace('like_button', partial: 'posts/likes', locals: { post: @post }) }
         format.html { redirect_to @post, notice: t('.success') }
       else
