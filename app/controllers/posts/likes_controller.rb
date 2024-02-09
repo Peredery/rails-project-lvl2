502ed2b4b2d83ce03f83fd0d13301ebdfe90e87b
A.V. Peredery <peredery@pm.me>
2024-02-09 14:37:19 +0500

    fix turbo likes

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -23,7 +23,7 @@ class Posts::LikesController < ApplicationController
 
     respond_to do |format|
       if @like&.destroy
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('like_button', partial: 'posts/likes', locals: { post: @post }) }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("like_button_post_id#{@post.id}", partial: 'posts/likes', locals: { post: @post }) }
         format.html { redirect_to @post, notice: t('.success') }
       else
         format.html { redirect_to @post, alert: t('.failure') }
