2e0a90b31b93e0384bda3e4321070314fdf41d7e
A.V. Peredery <peredery@pm.me>
2024-02-16 15:58:20 +0500

    fix(likes): remove format html

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -10,7 +10,6 @@ class Posts::LikesController < ApplicationController
     respond_to do |format|
       if @like.new_record? && @like.save
         format.turbo_stream { render turbo_stream: turbo_stream.replace("like_button_post_id#{@post.id}", partial: 'posts/likes', locals: { post: @post, user_like: @like }) }
-        format.html { redirect_to @post, notice: t('.success') }
       else
         format.html { redirect_to @post, alert: t('.failure') }
       end
@@ -24,7 +23,6 @@ class Posts::LikesController < ApplicationController
     respond_to do |format|
       if @like&.destroy
         format.turbo_stream { render turbo_stream: turbo_stream.replace("like_button_post_id#{@post.id}", partial: 'posts/likes', locals: { post: @post, user_like: nil }) }
-        format.html { redirect_to @post, notice: t('.success') }
       else
         format.html { redirect_to @post, alert: t('.failure') }
       end
