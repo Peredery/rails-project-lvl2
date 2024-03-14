a026165b3f878fde19c20832be5f41e619996050
A.V. Peredery <peredery@pm.me>
2024-03-14 21:41:18 +0500

    remove categories views and controller

--- app/controllers/categories_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class CategoriesController < ApplicationController
-  def show
-    @posts = Category.find_by(name: params[:name]).posts.order(created_at: :desc)
-    @user_likes = current_user ? current_user.likes.pluck(:post_id) : []
-  end
-end
